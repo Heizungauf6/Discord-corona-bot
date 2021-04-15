1ccfa4a609d53570018dcf5d2143f79d66721fbb
Heizungauf6 <alexanderfuchs04@gmail.com>
2021-04-15 12:25:11 +0200

    Check for Data
    Checks if data was fetched

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { token } = require('./token.json');
+const { prefix } = require('./config.json');
 fs = require("fs");
 
 
@@ -33,7 +34,7 @@ fs.readdir("./commands/", (err, files) => {
 
 client.once('ready', () => {
 	console.log(client.user.username + ' has successfully booted up.');
-	client.user.setPresence({ activity: { name: '?help' }, status: 'online' })
+	client.user.setPresence({ activity: { name: `${prefix}help` }, status: 'online' })
 });
 
 client.login(token);
