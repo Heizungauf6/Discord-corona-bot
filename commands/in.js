c14d76ddeade0d9e45ab59344ab570a68f4fa907
Heizungauf6 <alexanderfuchs04@gmail.com>
2021-05-17 23:27:22 +0200

    Optimised Code

--- commands/in.js
+++ commands/in.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { prefix } = require('../config.json');
 const fetch = require('node-fetch');
-const { getDistrictByNameAndType } = require("../node_modules/landkreise-deutschland/lib/index");
+const { getDistrictByNameAndType } = require("landkreise-deutschland");
 
 exports.run = async (client, message, args) => {
 
@@ -44,85 +44,6 @@ exports.run = async (client, message, args) => {
         .setTimestamp(); 
 
     let msg = await message.channel.send(embed);
-
-    await msg.react("📈")
-    await msg.react("🚫")
-    await msg.react("🗺️")
-
-
-    const filter = (reaction, user) => reaction.emoji.name === "📈" && user.id === message.author.id;
-    const filter1 = (reaction, user) => reaction.emoji.name === "🚫" && user.id === message.author.id;
-    const filter2 = (reaction, user) => reaction.emoji.name === "🗺️" && user.id === message.author.id;
-
-    const Diagram = msg.createReactionCollector(filter, {time: 60000, dispose: true});
-    const Maßnahmen = msg.createReactionCollector(filter1, {time: 60000, dispose: true});
-    const Heatmap = msg.createReactionCollector(filter2, {time: 60000, dispose: true});
-
-    Diagram.on("collect", async r => {
-
-        let getToday = async () => {
-            let responsetoday = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}`)
-            let In = await responsetoday.json()
-            return In
-        
-        }
-    
-        let getWeek = async () => {
-            let responseweek = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}/history/incidence/7`)
-            let In = await responseweek.json()
-            return In
-        
-        }
-
-        let InValue = await getToday()
-        let InValue2 = await getWeek()
-    
-        Weekdata = [InValue2.data[`${district.AGS}`].history[0].weekIncidence, InValue2.data[`${district.AGS}`].history[1].weekIncidence, InValue2.data[`${district.AGS}`].history[2].weekIncidence, InValue2.data[`${district.AGS}`].history[3].weekIncidence, InValue2.data[`${district.AGS}`].history[4].weekIncidence, InValue2.data[`${district.AGS}`].history[5].weekIncidence, InValue2.data[`${district.AGS}`].history[6].weekIncidence]
-    
-        const chart = {
-            type: 'bar',
-            data: {
-              labels: ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5', 'Tag 6', 'Heute'],
-              datasets: [{
-                label: 'Inzidenz',
-                data: Weekdata
-              }
-              ]
-            }
-          }
-    
-    
-    
-          const encodedChart = encodeURIComponent(JSON.stringify(chart));
-          const chartUrl = `https://quickchart.io/chart?c=${encodedChart}`;
-
-            embed.setColor('#EFFF00')
-            embed.fields = [];
-            embed.image = [];
-            embed.setTitle(`${district.type} ${district.name}, Diagramm`)
-            embed.setImage(chartUrl)
-            embed.setTimestamp(); 
-    
-            msg.edit(embed);
-    })
-
-    Maßnahmen.on("collect", r => {
-        embed.setTitle(`${district.type} ${district.name}, Maßnahmen`)
-        embed.fields = [];
-        embed.image = [];
-        embed.addFields(
-            { name: 'Maßnahmen: ', value: `Wir werden alle Sterben`},
-            { name: 'Quelle: ', value: `ndr.de`}
-        )
-        msg.edit(embed);
-    })
-
-    Heatmap.on("collect", r => {
-        embed.setTitle(`Heatmap`);
-        embed.fields = [];
-        embed.setImage('https://api.corona-zahlen.org/map/districts')
-        msg.edit(embed);
-    })
     }
 
     if(city == false && county == true){
@@ -143,84 +64,7 @@ exports.run = async (client, message, args) => {
         .setFooter('📈 Diagram 🚫 Maßnahmen 🗺️ Heatmap')
         .setTimestamp(); 
 
-    let msg = await message.channel.send(embed);
-
-    await msg.react("📈")
-    await msg.react("🚫")
-    await msg.react("🗺️")
-
-
-    const filter = (reaction, user) => reaction.emoji.name === "📈" && user.id === message.author.id;
-    const filter1 = (reaction, user) => reaction.emoji.name === "🚫" && user.id === message.author.id;
-    const filter2 = (reaction, user) => reaction.emoji.name === "🗺️" && user.id === message.author.id;
-
-    const Diagram = msg.createReactionCollector(filter, {time: 60000, dispose: true});
-    const Maßnahmen = msg.createReactionCollector(filter1, {time: 60000, dispose: true});
-    const Heatmap = msg.createReactionCollector(filter2, {time: 60000, dispose: true});
-
-    Diagram.on("collect", async r => {
-    
-        let getWeek = async () => {
-            let responseweek = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}/history/incidence/7`)
-            let In = await responseweek.json()
-            return In
-        
-        }
-
-
-        let InValue2 = await getWeek()
-
-        if (InValue2.data == null) {
-            message.channel.send(`Das RKI übermittelt derzeit keine Daten, ${message.author}!`);
-            return;
-          }
-    
-        Weekdata = [InValue2.data[`${district.AGS}`].history[0].weekIncidence, InValue2.data[`${district.AGS}`].history[1].weekIncidence, InValue2.data[`${district.AGS}`].history[2].weekIncidence, InValue2.data[`${district.AGS}`].history[3].weekIncidence, InValue2.data[`${district.AGS}`].history[4].weekIncidence, InValue2.data[`${district.AGS}`].history[5].weekIncidence, InValue2.data[`${district.AGS}`].history[6].weekIncidence]
-    
-        const chart = {
-            type: 'bar',
-            data: {
-              labels: ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5', 'Tag 6', 'Heute'],
-              datasets: [{
-                label: 'Inzidenz',
-                data: Weekdata
-              }
-              ]
-            }
-          }
-    
-    
-    
-          const encodedChart = encodeURIComponent(JSON.stringify(chart));
-          const chartUrl = `https://quickchart.io/chart?c=${encodedChart}`;
-
-            embed.setColor('#EFFF00')
-            embed.fields = [];
-            embed.image = [];
-            embed.setTitle(`${district.type} ${district.name}, Diagramm`)
-            embed.setImage(chartUrl)
-            embed.setTimestamp(); 
-    
-            msg.edit(embed);
-    })
-
-    Maßnahmen.on("collect", r => {
-        embed.setTitle(`${district.type} ${district.name}, Maßnahmen`)
-        embed.fields = [];
-        embed.image = [];
-        embed.addFields(
-            { name: 'Maßnahmen: ', value: `Wir werden alle Sterben`},
-            { name: 'Quelle: ', value: `ndr.de`}
-        )
-        msg.edit(embed);
-    })
-
-    Heatmap.on("collect", r => {
-        embed.setTitle(`Heatmap`);
-        embed.fields = [];
-        embed.setImage('https://api.corona-zahlen.org/map/districts')
-        msg.edit(embed);
-    })
+    let msg = await message.channel.send(embed)
     }
 
     if (city == true && county == true) {
@@ -245,7 +89,11 @@ exports.run = async (client, message, args) => {
 
         citycheck.on("collect", async r => {
             msg.reactions.removeAll();
-            const district = getDistrictByNameAndType(`${args[0]}`, `Stadt`);
+
+            await msg.react("📈")
+            await msg.react("🗺️")
+
+            district = getDistrictByNameAndType(`${args[0]}`, `Stadt`);
             let GetCity = async () => {
                 let response = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}`)
                 let In = await response.json()
@@ -261,94 +109,20 @@ exports.run = async (client, message, args) => {
             embed.addFields(
                 { name: 'Inzidenz', value: someint.toFixed(2) },
                 { name: 'Quelle', value: `${InValue.meta.source}` })
-            embed.setFooter('📈 Diagram 🚫 Maßnahmen 🗺️ Heatmap')
+            embed.setFooter('📈 Diagram 🗺️ Heatmap')
             embed.setTimestamp();
 
             msg.edit(embed);
 
-            await msg.react("📈")
-    await msg.react("🚫")
-    await msg.react("🗺️")
-
-
-    const filter = (reaction, user) => reaction.emoji.name === "📈" && user.id === message.author.id;
-    const filter1 = (reaction, user) => reaction.emoji.name === "🚫" && user.id === message.author.id;
-    const filter2 = (reaction, user) => reaction.emoji.name === "🗺️" && user.id === message.author.id;
-
-    const Diagram = msg.createReactionCollector(filter, {time: 60000, dispose: true});
-    const Maßnahmen = msg.createReactionCollector(filter1, {time: 60000, dispose: true});
-    const Heatmap = msg.createReactionCollector(filter2, {time: 60000, dispose: true});
-
-    Diagram.on("collect", async r => {
-
-        let getToday = async () => {
-            let responsetoday = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}`)
-            let In = await responsetoday.json()
-            return In
-        
-        }
-    
-        let getWeek = async () => {
-            let responseweek = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}/history/incidence/7`)
-            let In = await responseweek.json()
-            return In
-        
-        }
-
-        let InValue = await getToday()
-        let InValue2 = await getWeek()
-    
-        Weekdata = [InValue2.data[`${district.AGS}`].history[0].weekIncidence, InValue2.data[`${district.AGS}`].history[1].weekIncidence, InValue2.data[`${district.AGS}`].history[2].weekIncidence, InValue2.data[`${district.AGS}`].history[3].weekIncidence, InValue2.data[`${district.AGS}`].history[4].weekIncidence, InValue2.data[`${district.AGS}`].history[5].weekIncidence, InValue2.data[`${district.AGS}`].history[6].weekIncidence]
-    
-        const chart = {
-            type: 'bar',
-            data: {
-              labels: ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4', 'Tag 5', 'Tag 6', 'Heute'],
-              datasets: [{
-                label: 'Inzidenz',
-                data: Weekdata
-              }
-              ]
-            }
-          }
-    
-    
-    
-          const encodedChart = encodeURIComponent(JSON.stringify(chart));
-          const chartUrl = `https://quickchart.io/chart?c=${encodedChart}`;
-
-            embed.setColor('#EFFF00')
-            embed.fields = [];
-            embed.image = [];
-            embed.setTitle(`${district.type} ${district.name}, Diagramm`)
-            embed.setImage(chartUrl)
-            embed.setTimestamp(); 
-    
-            msg.edit(embed);
-    })
-
-    Maßnahmen.on("collect", r => {
-        embed.setTitle(`${district.type} ${district.name}, Maßnahmen`)
-        embed.fields = [];
-        embed.image = [];
-        embed.addFields(
-            { name: 'Maßnahmen: ', value: `Wir werden alle Sterben`},
-            { name: 'Quelle: ', value: `ndr.de`}
-        )
-        msg.edit(embed);
-    })
-
-    Heatmap.on("collect", r => {
-        embed.setTitle(`Heatmap`);
-        embed.fields = [];
-        embed.setImage('https://api.corona-zahlen.org/map/districts')
-        msg.edit(embed);
-    })
         })
 
         countycheck.on("collect", async r => {
             msg.reactions.removeAll();
-            const district = getDistrictByNameAndType(`${args[0]}`, `Landkreis`);
+
+            await msg.react("📈")
+            await msg.react("🗺️")
+
+            district = getDistrictByNameAndType(`${args[0]}`, `Landkreis`);
             let getIn = async () => {
                 let response = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}`)
                 let In = await response.json()
@@ -364,33 +138,24 @@ exports.run = async (client, message, args) => {
             embed.addFields(
                 { name: 'Inzidenz', value: someint.toFixed(2) },
                 { name: 'Quelle', value: `${InValue.meta.source}` })
-            embed.setFooter('📈 Diagram 🚫 Maßnahmen 🗺️ Heatmap')
+            embed.setFooter('📈 Diagram 🗺️ Heatmap')
             embed.setTimestamp();
 
             msg.edit(embed);
 
-            await msg.react("📈")
-    await msg.react("🚫")
-    await msg.react("🗺️")
+
+        })
+
 
 
     const filter = (reaction, user) => reaction.emoji.name === "📈" && user.id === message.author.id;
-    const filter1 = (reaction, user) => reaction.emoji.name === "🚫" && user.id === message.author.id;
     const filter2 = (reaction, user) => reaction.emoji.name === "🗺️" && user.id === message.author.id;
 
     const Diagram = msg.createReactionCollector(filter, {time: 60000, dispose: true});
-    const Maßnahmen = msg.createReactionCollector(filter1, {time: 60000, dispose: true});
     const Heatmap = msg.createReactionCollector(filter2, {time: 60000, dispose: true});
 
     Diagram.on("collect", async r => {
 
-        let getToday = async () => {
-            let responsetoday = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}`)
-            let In = await responsetoday.json()
-            return In
-        
-        }
-    
         let getWeek = async () => {
             let responseweek = await fetch(`https://api.corona-zahlen.org/districts/${district.AGS}/history/incidence/7`)
             let In = await responseweek.json()
@@ -398,7 +163,6 @@ exports.run = async (client, message, args) => {
         
         }
 
-        let InValue = await getToday()
         let InValue2 = await getWeek()
     
         Weekdata = [InValue2.data[`${district.AGS}`].history[0].weekIncidence, InValue2.data[`${district.AGS}`].history[1].weekIncidence, InValue2.data[`${district.AGS}`].history[2].weekIncidence, InValue2.data[`${district.AGS}`].history[3].weekIncidence, InValue2.data[`${district.AGS}`].history[4].weekIncidence, InValue2.data[`${district.AGS}`].history[5].weekIncidence, InValue2.data[`${district.AGS}`].history[6].weekIncidence]
@@ -430,25 +194,12 @@ exports.run = async (client, message, args) => {
             msg.edit(embed);
     })
 
-    Maßnahmen.on("collect", r => {
-        embed.setTitle(`${district.type} ${district.name}, Maßnahmen`)
-        embed.fields = [];
-        embed.image = [];
-        embed.addFields(
-            { name: 'Maßnahmen: ', value: `Wir werden alle Sterben`},
-            { name: 'Quelle: ', value: `ndr.de`}
-        )
-        msg.edit(embed);
-    })
-
     Heatmap.on("collect", r => {
         embed.setTitle(`Heatmap`);
         embed.fields = [];
         embed.setImage('https://api.corona-zahlen.org/map/districts')
         msg.edit(embed);
     })
-
-        })
 
 
         
